118aaad068c5cb33dbfe25ae21b3f41301192282
BigBlueHat <byoung@bigbluehat.com>
2023-08-30 16:11:02 -0400

    Remove mongoose & database config.

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const { app, PORT } = require('./app');
-const dbConnection = require('./config/db');
 
 const bootstrap = () => {
   app.listen(PORT, () => {
-    dbConnection();
     console.log(`app is listening on http://localhost:${PORT}..`);
   });
 };
