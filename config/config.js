b9849997eaf4675c908e560c19194d94588299b3
Nate Otto <nate@ottonomy.net>
2023-09-26 11:19:51 -0700

    Add configuration file and test unknown client_id

--- config/config.js
+++ config/config.js
@@ -1,9 +1,95 @@
 import * as dotenv from 'dotenv';
+import * as fs from 'fs';
+import * as yaml from 'js-yaml';
 
+// Environment variables
 dotenv.config();
+const config_path = process.env.CONFIG_PATH || 'config.yaml';
 
-// export any globals, etc. here as needed
-export const client_id = process.env.CLIENT_ID;
-export const client_secret = process.env.CLIENT_SECRET;
-export const redirect_uri = process.env.REDIRECT_URI;
-export const issuer = process.env.ISSUER;
+// Load config doc and parse YAML.
+const configDoc = yaml.load(fs.readFileSync(config_path, 'utf8'));
+
+export const relyingParties = configDoc.relying_parties;
+
+const validateRelyingParty = rp => {
+  if(!rp.client_id) {
+    throw new Error('client_id is required for each configured relying_party');
+  }
+  if(!rp.client_secret) {
+    throw new Error(`client_secret is required in ${rp.client_id}`);
+  }
+
+  // if redirect_uri doesn't match http or https throw an error
+  if(!rp.redirect_uri.match(/^https?:\/\//)) {
+    throw new Error(`redirect_uri must be a URI in client ${rp.client_id}`);
+  }
+
+  if(!rp.scopes || !Array.isArray(rp.scopes)) {
+    throw new Error(
+      `An array of scopes must be defined io client ${rp.client_id}.`
+    );
+  }
+  if(!rp.scopes.map(s => s.name).includes('openid')) {
+    throw new Error(`scopes in client ${rp.client_id} must include openid`);
+  }
+
+  // If credential_context is not a string, throw an error
+  if(
+    !rp.credential_context ||
+    typeof rp.credential_context !== 'string' ||
+    !rp.credential_context.match(/^https?:\/\//)
+  ) {
+    throw new Error(
+      `credential_context must be a URL in client ${rp.client_id}`
+    );
+  }
+
+  // If credential_type is not a string throw an error
+  if(
+    !rp.credential_type ||
+    typeof rp.credential_type !== 'string' ||
+    rp.credential_type.length === 0
+  ) {
+    throw new Error(
+      `credential_type must be defined in client ${rp.client_id}`
+    );
+  }
+
+  // Credential issuer must be a string
+  if(
+    !rp.credential_issuer ||
+    typeof rp.credential_issuer !== 'string' ||
+    rp.credential_issuer.length === 0
+  ) {
+    throw new Error(
+      `credential_issuer must be defined in client ${rp.client_id}`
+    );
+  }
+
+};
+
+// If relyingParties is not an array, throw an error
+if(!Array.isArray(relyingParties)) {
+  throw new Error('Configuration relying_parties must be an array.');
+}
+
+relyingParties.forEach(validateRelyingParty);
+export const exchanger = configDoc.exchanger;
+export const defaultLanguage = configDoc.default_language || 'en';
+
+const defaultTranslations = {
+  en: {
+    login_cta: 'Login with your credential wallet',
+    login_explain: 'To login with your credential wallet, you will need ' +
+      'to have the credential wallet app <with configurable URL to app ' +
+      'stores> installed',
+    app_install_explain: 'If you don\'t have a credential wallet yet, ' +
+      'you can get one by downloading the credential wallet app ' +
+      '<with configurable URL to app stores>',
+    app_cta: 'Open wallet app',
+    qr_explain: 'Looking for a QR Code to scan with you wallet app instead?',
+    qr_cta: 'Scan the following QR Code with your wallet app',
+    copyright: 'Powered by OpenCred'
+  }
+};
+export const translations = configDoc.translations || defaultTranslations;
--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,4 @@
-import {client_id} from '../config/config.js';
+import {relyingParties} from '../config/config.js';
 
 export async function exchangeCodeForToken(req, res) {
   res.status(500).send('Not implemented');
@@ -6,7 +6,9 @@ export async function exchangeCodeForToken(req, res) {
 
 export async function login(req, res) {
   // Validate the client_id parameter from the request
-  if(req.query.client_id != client_id) {
+
+  // If the client_id is not in the relyingParties array, throw an error
+  if(!relyingParties.map(rp => rp.client_id).includes(req.query.client_id)) {
     res.status(400).send({message: 'Unknown client_id'});
   }
 
