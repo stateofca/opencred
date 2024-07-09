54042af704ce27faa45ae84be42e2ddbbd2f6911
Nate Otto <nate@ottonomy.net>
2024-07-09 11:07:46 -0700

    Adjust error messaging and config loading order

--- dev.js
+++ dev.js
@@ -7,22 +7,30 @@
 import * as bedrock from '@bedrock/core';
 import * as fs from 'node:fs';
 import {fileURLToPath} from 'node:url';
+import {logger} from './lib/logger.js';
 import path from 'node:path';
 
 const {config} = bedrock;
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-try {
-  const configPath = path.join(__dirname, 'configs/combined.yaml');
-  console.log('Loading config from ' + configPath);
-  const localConfig = fs.readFileSync(configPath, 'utf8');
-  const based = Buffer.from(localConfig).toString('base64');
-  process.env.BEDROCK_CONFIG = based;
-  console.log(based.slice(0, 40));
-} catch(e) {
-  console.log('Failed to load a local config.');
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const configPath = path.join(__dirname, 'configs/combined.yaml');
+
+let localConfig = '';
+
+if(!process.env.BEDROCK_CONFIG) {
+  try {
+    logger.info('Loading config from ' + configPath);
+    localConfig = fs.readFileSync(configPath, 'utf8');
+  } catch(e) {
+    logger.warn('Failed to load config from ' + configPath);
+  }
 }
 
+if(!!localConfig) {
+  const based = Buffer.from(localConfig).toString('base64');
+  process.env.BEDROCK_CONFIG = based;
+  logger.info('Loaded config from ' + configPath);
+}
 config.paths.config = path.join(__dirname, 'configs');
 import './lib/index.js';
 
