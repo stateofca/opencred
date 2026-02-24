aa1024a787dc4a793adc8f337f9a465c013870b6
Nate Otto <nate@ottonomy.net>
2026-02-23 17:44:34 -0800

    chore(lint): upgrade eslint and @digitalbazaar/eslint-config

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+/*!
+ * Copyright 2023 - 2024 California Department of Motor Vehicles
+ * Copyright 2023 - 2026 Digital Bazaar, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+import nodeConfig from '@digitalbazaar/eslint-config/node-recommended';
+import vue3Config from '@digitalbazaar/eslint-config/vue3-recommended';
+
+export default [
+  // quasar not yet supported
+  // 'plugin:quasar/standard',
+  ...nodeConfig,
+  ...vue3Config,
+  {
+    rules: {
+      'vue/no-v-html': 'off'
+    }
+  }
+];
