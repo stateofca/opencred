9e4866e8a2456d8640a99b50bc1462ce79e20ad4
Nate Otto <nate@ottonomy.net>
2026-02-23 16:30:43 -0800

    chore(test): testing-improvement - phase 1: setup test:unit and test:bedrock structure

---
+++ test/unit/00-placeholder.test.js
@@ -0,0 +1,15 @@
+/*!
+ * Copyright 2023 - 2025 California Department of Motor Vehicles
+ * Copyright 2023 - 2025 Digital Bazaar, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * Placeholder until unit tests are migrated from test/mocha in Phase 2/3.
+ * Remove when test/unit/ has real tests.
+ */
+
+describe('Unit tests', () => {
+  it('placeholder - unit tests will be migrated in Phase 2/3', () => {
+    // No-op
+  });
+});
